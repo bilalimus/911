607fe26fe5b44cec5bd242db3975d2d756ca24a5
Omuraliev Maksatbek <bilalimus@gmail.com>
2024-02-14 03:39:44 +0600

    Links are changed to box from dropbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-const myApps = [
-  {
-    name: "Activator",
-    link: "https://www.dropbox.com/s/iq6of62iqhfi2m5/KMSAuto_Net_Office_111.zip?dl=0",
-  },
-  {
-    name: "VNC",
-    link: "https://www.dropbox.com/scl/fi/1rf7gm7w0zyo44tklvfge/UltraVNC64.zip?rlkey=mf46tcgchj2ly5e3g00ghw3u6&dl=0",
-  },
-];
+import { myApps } from "./Links";
 
 function App() {
   return (
---
+++ src/Links.js
@@ -0,0 +1,10 @@
+export const myApps = [
+    {
+      name: "Activator",
+      link: "https://app.box.com/s/nxeq87xepchqe4t1saw60yj2p8jylcf2",
+    },
+    {
+      name: "VNC",
+      link: "https://app.box.com/s/ejkunvp72dlnewvtap6h3c1okic98rew",
+    },
+  ];
