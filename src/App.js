68177166406184c3b398fa3ad2c0608d5341f9d7
Omuraliev Maksatbek <bilalimus@gmail.com>
2024-02-14 03:05:46 +0600

    2 links added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const myApps = [
+  {
+    name: "Activator",
+    link: "https://www.dropbox.com/s/iq6of62iqhfi2m5/KMSAuto_Net_Office_111.zip?dl=0",
+  },
+  {
+    name: "VNC",
+    link: "https://www.dropbox.com/scl/fi/1rf7gm7w0zyo44tklvfge/UltraVNC64.zip?rlkey=mf46tcgchj2ly5e3g00ghw3u6&dl=0",
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>List of links</p>
+        {myApps.map((app) => {
+          return (
+            <a
+              className="App-link"
+              href={app.link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {app.name}
+            </a>
+          );
+        })}
       </header>
     </div>
   );
