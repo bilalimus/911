627bd3f70a77168896cb7e86b01298f907c2822c
TalaABA <352018@mail.ru>
2024-04-01 14:52:54 +0600

    911

--- src/Links.js
+++ src/Links.js
@@ -1,4 +1,8 @@
 export const myApps = [
+  {
+    name: "911",
+      link: "https://app.box.com/s/d67e1zu2nhr82s4k4va0gip2r3hu0l1c",
+  },
     {
       name: "Activator",
       link: "https://app.box.com/s/nxeq87xepchqe4t1saw60yj2p8jylcf2",
@@ -12,3 +16,5 @@ export const myApps = [
     link: "https://drive.google.com/drive/folders/1A7H5HDB1dTiT3h-JOE_KrIsxxt6siIcp?usp=sharing",
   },
   ];
+
+  <a class="App-link" href="https://app.box.com/s/d67e1zu2nhr82s4k4va0gip2r3hu0l1c" target="_blank" rel="noopener noreferrer">911</a>
