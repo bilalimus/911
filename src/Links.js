209ce976807e714f2a5dd68f69e81a469bc74a01
Maximus on Victus <bilalimus@gmail.com>
2024-08-12 15:29:18 +0600

    Added new Activator

--- src/Links.js
+++ src/Links.js
@@ -1,20 +1,31 @@
 export const myApps = [
   {
     name: "911",
-      link: "https://app.box.com/s/d67e1zu2nhr82s4k4va0gip2r3hu0l1c",
+    link: "https://app.box.com/s/d67e1zu2nhr82s4k4va0gip2r3hu0l1c",
   },
-    {
-      name: "Activator",
-      link: "https://app.box.com/s/nxeq87xepchqe4t1saw60yj2p8jylcf2",
-    },
-    {
-      name: "VNC",
-      link: "https://app.box.com/s/ejkunvp72dlnewvtap6h3c1okic98rew",
-    },
+  {
+    name: "Activator",
+    link: "https://app.box.com/s/nxeq87xepchqe4t1saw60yj2p8jylcf2",
+  },
+  {
+    name: "VNC",
+    link: "https://app.box.com/s/ejkunvp72dlnewvtap6h3c1okic98rew",
+  },
   {
     name: "1C",
     link: "https://drive.google.com/drive/folders/1A7H5HDB1dTiT3h-JOE_KrIsxxt6siIcp?usp=sharing",
   },
-  ];
+  {
+    name: "Activator-30.01.2024",
+    link: "https://terabox.com/s/15rlBo21_Mv4pD4uctUIULA",
+  },
+];
 
-  <a class="App-link" href="https://app.box.com/s/d67e1zu2nhr82s4k4va0gip2r3hu0l1c" target="_blank" rel="noopener noreferrer">911</a>
+<a
+  class="App-link"
+  href="https://app.box.com/s/d67e1zu2nhr82s4k4va0gip2r3hu0l1c"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  911
+</a>;
